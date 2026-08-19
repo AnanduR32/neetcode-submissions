4a4dc9bc5e4ff30df82b9861a3742411031e4962
Anandu R <32507674+AnanduR32@users.noreply.github.com>
2026-08-19 14:08:19 +0530

    Add: time-based-key-value-store - submission-16

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-16.py
@@ -0,0 +1,30 @@
+class TimeMap:
+
+    def __init__(self):
+        self.maxStamps = dict()
+        self.stamps = dict()
+
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.stamps:
+            self.stamps[key] = list()
+        self.stamps[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.stamps:
+            return ''
+        low = 0
+        high = len(self.stamps[key]) - 1
+        possible_idx = -1
+        if key in self.stamps:
+            while low <= high:
+                mid = (low + high) // 2
+                if self.stamps[key][mid][0] == timestamp:
+                    return self.stamps[key][mid][1]
+                if self.stamps[key][mid][0] < timestamp:
+                    possible_idx = mid
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return self.stamps[key][possible_idx][1] if possible_idx >= 0 else ""
+
